6d1c1e031511389901d412fa23dba1ffad80aca7
Nathan Glazebrook <nathanglazebrook@Nathans-iMac.local>
2022-10-04 18:12:20 -0600

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("I would like to be able to efficiently construct user-friendly web sites and work from home to be able to provide additional income. My favorite hobby is woodcarving." );
+let firstName = prompt("What's your first name");
+console.log(firstName);
+let lastName = prompt("what's your last name");
+console.log(lastName);
+alert(lastName);
+let birthday = prompt("what is your birthday");
+console.log(birthday);
+let userConfirmation = confirm(`is this your birthday: ${birthday}`);
+alert(userConfirmation);
