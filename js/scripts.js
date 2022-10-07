38c1f12503810878b2757481a8afaf1b317f9ecd
Nathan Glazebrook <nathanglazebrook@Nathans-iMac.local>
2022-10-07 16:31:06 -0600

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log("I would like to be able to efficiently construct user-friendly web sites and work from home to be able to provide additional income. My favorite hobby is woodcarving." );
+console.log("36" === 36);
+console.log("110" != 110);
+let testString = "25";
+let objectString = new String("25");
+console.log(testString , objectString);
+let age = true;
+console.log(parseInt(age));
+if (testString === objectString){
+    console.log(testString , objectString);
+} else {
+    console.log("it was false");
+}
+var x = prompt("Enter a Value", "10");
+var y = prompt("Enter a Value", "10");
+var num1 = parseInt(x);
+ var num2 = parseInt(y);
+ alert("10+10=20");
